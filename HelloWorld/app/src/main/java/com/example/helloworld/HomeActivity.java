bc4a3658b6beadeb8dba5d5caa4ca376a71bcd57
Fransiskusdeli <fransiskus.deli@ti.ukdw.ac.id>
2020-10-19 15:49:22 +0700

    Master commit (T5)

--- HelloWorld/app/src/main/java/com/example/helloworld/HomeActivity.java
+++ HelloWorld/app/src/main/java/com/example/helloworld/HomeActivity.java
@@ -4,13 +4,26 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 
 import android.annotation.SuppressLint;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.wifi.WifiManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.widget.CompoundButton;
+import android.widget.Switch;
 
 import com.google.android.material.tabs.TabItem;
 import com.google.android.material.tabs.TabLayout;
 
 
 public class HomeActivity extends AppCompatActivity {
+    private Switch WifiSwi;
+    private WifiManager WifiManager;
+    private WifiNotif wifiNotif;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,6 +55,88 @@ public class HomeActivity extends AppCompatActivity {
             @Override
             public void onTabReselected(TabLayout.Tab tab) {
 
+            }
+        });
+
+        WifiSwi = findViewById(R.id.switchwifi);
+        wifiNotif = new WifiNotif();
+//        WifiManager = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+//        WifiSwi.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+//            @Override
+//            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+//                if(isChecked){
+//                    WifiManager.setWifiEnabled(true);
+//                    WifiSwi.setText("Wifi ON");
+//
+//                }else {
+//                    WifiManager.setWifiEnabled(false);
+//                    WifiSwi.setText("Wifi OFF");
+//                }
+//            }
+//        });
+        inisialisasiBroadcastRec();
+
+    }
+
+
+        protected void onStart(){
+        super.onStart();
+            IntentFilter intentFilter = new IntentFilter(WifiManager.WIFI_STATE_CHANGED_ACTION);
+            registerReceiver(WifiStateRec, intentFilter);
+        }
+
+        @Override
+        protected void onStop() {
+            super.onStop();
+            unregisterReceiver(WifiStateRec);
+        }
+
+    private BroadcastReceiver WifiStateRec = new BroadcastReceiver() {
+
+        @Override
+
+            public void onReceive(Context context, Intent intent) {
+                int extra = intent.getIntExtra(WifiManager.EXTRA_WIFI_STATE, WifiManager.WIFI_STATE_UNKNOWN);
+
+                switch (extra){
+                    case android.net.wifi.WifiManager.WIFI_STATE_ENABLED:
+                        WifiSwi.setChecked(true);
+                        WifiSwi.setText("Wifi ON");
+                        //notif on
+                        wifiNotif.notifOn("Sudah terkoneksi", context);
+                        break;
+                    case android.net.wifi.WifiManager.WIFI_STATE_DISABLED:
+                        WifiSwi.setChecked(false);
+                        WifiSwi.setText("Wifi OFF");
+                        //notif off
+                        wifiNotif.notifOn("Tidak terkoneksi", context);
+                        break;
+                }
+
+
+            }
+        };
+
+
+    private void inisialisasiBroadcastRec(){
+        WifiManager wifiManager = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+        WifiSwi.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            if(isChecked && !wifiManager.isWifiEnabled()){
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                    Intent panelIntent = new Intent(Settings.Panel.ACTION_WIFI);
+                    startActivityForResult(panelIntent, 1);
+                } else {
+                    wifiManager.setWifiEnabled(true);
+                }
+            }
+
+            else if(!isChecked && wifiManager.isWifiEnabled()){
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                    Intent panelIntent = new Intent(Settings.Panel.ACTION_WIFI);
+                    startActivityForResult(panelIntent, 1);
+                } else {
+                    wifiManager.setWifiEnabled(false);
+                }
             }
         });
     }
