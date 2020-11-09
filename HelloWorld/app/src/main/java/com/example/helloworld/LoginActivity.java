071e60b73fe47c2705ceec80066eab21c8bf2ab2
Fransiskusdeli <fransiskus.deli@ti.ukdw.ac.id>
2020-11-09 23:11:15 +0700

    Master commit (T7)

--- HelloWorld/app/src/main/java/com/example/helloworld/LoginActivity.java
+++ HelloWorld/app/src/main/java/com/example/helloworld/LoginActivity.java
@@ -1,9 +1,12 @@
 package com.example.helloworld;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.database.Cursor;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.text.Html;
+import android.text.Spanned;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -16,30 +19,52 @@ public class LoginActivity extends AppCompatActivity {
     private EditText txtUsername;
     private EditText txtPassword;
     private Button btnlogin;
+    private Button btnRegister;
+    DatabaseHelper dbHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.llogin);
-        txtUsername = findViewById(R.id.txtUsername);
-        txtPassword = findViewById(R.id.txtPassword);
-        btnlogin = findViewById(R.id.btnlogin);
+        txtUsername = findViewById(R.id.usernameLogin);
+        txtPassword = findViewById(R.id.passwordLogin);
+        btnRegister = findViewById(R.id.btnRegisterLogin);
+        btnlogin = findViewById(R.id.btnloginLogin);
+        dbHelper = new DatabaseHelper(this);
+//        TextView tvCreateAccount = (TextView)findViewById(R.id.tvCreateAccount);
+
         btnlogin.setOnClickListener(new View.OnClickListener() {
             @Override
 
             public void onClick(View v) {
-                if(txtPassword.getText().toString().equals("admin") && txtUsername.getText().toString().equals("admin")){
+//                if(txtPassword.getText().toString().equals("admin") && txtUsername.getText().toString().equals("admin")){
+//
+//                    Intent i = new Intent( LoginActivity.this, HomeActivity.class);
+//                    Toast.makeText(getApplicationContext(), "Welcome "+ txtUsername.getText(),   Toast.LENGTH_LONG).show();
+//                    startActivity(i);
+//                }
+//                else{
+//                    Toast.makeText(getApplicationContext(),"Tidak ditemukan "+ txtUsername.getText(), Toast.LENGTH_LONG).show();
+//                }
+                String usernamevalue = txtUsername.getText().toString();
+                String passwordvalue = txtPassword.getText().toString();
+                Boolean res = dbHelper.checkUser(usernamevalue,passwordvalue);
+                if(res == true){
+                    Toast.makeText(LoginActivity.this, "Login Successful", Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(LoginActivity.this, HomeActivity.class));
+                }else {
+                    Toast.makeText(LoginActivity.this, "Gagal", Toast.LENGTH_SHORT).show();
+                }
 
-                    Intent i = new Intent( LoginActivity.this, HomeActivity.class);
-                    Toast.makeText(getApplicationContext(), "Welcome "+ txtUsername.getText(),   Toast.LENGTH_LONG).show();
-                    startActivity(i);
-                }
-                else{
-                    Toast.makeText(getApplicationContext(),"Tidak ditemukan "+ txtUsername.getText(), Toast.LENGTH_LONG).show();
-                }
             }
-        }
-        );
+        });
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                 Intent intent= new Intent(LoginActivity.this, RegisterActivity.class);
+                 startActivity(intent);
+            }
+        });
     }
 }
 
---
+++ HelloWorld/app/src/main/java/com/example/helloworld/RegisterActivity.java
@@ -0,0 +1,66 @@
+package com.example.helloworld;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+import android.content.ContentValues;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.text.Html;
+import android.text.Spanned;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    EditText TxUsername, TxPassword, TxConPassword;
+    Button BtnRegister, BtnCancel;
+    DatabaseHelper dbHelper;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        dbHelper = new DatabaseHelper(this);
+
+        TxUsername = (EditText)findViewById(R.id.emailusernameReg);
+        TxPassword = (EditText)findViewById(R.id.passwordReg);
+        BtnRegister = (Button)findViewById(R.id.btnRegisterReg);
+        BtnCancel = (Button)findViewById(R.id.btnCancelReg);
+
+        BtnCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent= new Intent(RegisterActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        BtnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = TxUsername.getText().toString().trim();
+                String password = TxPassword.getText().toString().trim();
+                ContentValues values = new ContentValues();
+
+                if (password.equals("") || username.equals("")){
+                    Toast.makeText(RegisterActivity.this, "Password dan username tidak boleh kosong!", Toast.LENGTH_SHORT).show();
+                }else {
+                    values.put(DatabaseHelper.row_username, username);
+                    values.put(DatabaseHelper.row_password, password);
+                    dbHelper.insertData(values);
+
+                    Toast.makeText(RegisterActivity.this, "Berhasil Register!", Toast.LENGTH_SHORT).show();
+                    finish();
+                }
+            }
+        });
+
+
+    }
+}
